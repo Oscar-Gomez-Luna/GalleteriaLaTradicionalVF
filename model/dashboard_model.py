7806f5ca526f75c2816dd7472f414c43761b6cca
IDGS-901-22000823 <gutierrez.ascencio.luis@gmail.com>
2025-08-27 00:50:03 -0600

    Update dashboard_model.py

--- model/dashboard_model.py
+++ model/dashboard_model.py
@@ -1,21 +1,21 @@
 # model/dashboard_model.py
 from extensions import db
 from datetime import datetime, timedelta
-from model.venta import Venta  # Actualizado
-from model.detalle_venta import DetalleVentaGalletas  # Actualizado
-from model.lote_galleta import LoteGalletas  # Actualizado
-from model.galleta import Galleta  # Actualizado
-from model.tipo_galleta import TipoGalleta  # Actualizado
+from model.venta import Venta
+from model.detalle_venta import DetalleVentaGalletas
+from model.lote_galleta import LoteGalletas
+from model.galleta import Galleta
+from model.tipo_galleta import TipoGalleta
 
 def get_ventas_diarias():
     """
-    Obtiene las ventas diarias de los últimos 7 días.
-    Retorna una lista de fechas y una lista de totales.
+    Obtiene las ventas diarias de los últimos 7 días
+    lista de fechas y una lista de totales
     """
     end_date = datetime.now().date()
     start_date = end_date - timedelta(days=7)
 
-    # Usar el ORM para obtener las ventas diarias
+    # obtener las ventas diarias
     result = (db.session.query(Venta.fecha, db.func.sum(Venta.total).label('total_ventas'))
               .filter(Venta.fecha.between(start_date, end_date))
               .group_by(Venta.fecha)
@@ -28,8 +28,8 @@ def get_ventas_diarias():
 
 def get_productos_mas_vendidos():
     """
-    Obtiene los 5 productos más vendidos.
-    Retorna una lista de nombres de productos y una lista de cantidades vendidas.
+    Obtiene los 5 productos más vendidos
+    lista de nombres de productos y una lista de cantidades vendidas
     """
     # Usar el ORM para obtener los productos más vendidos
     result = (db.session.query(Galleta.galleta, db.func.sum(DetalleVentaGalletas.cantidad).label('cantidad_vendida'))
@@ -46,10 +46,10 @@ def get_productos_mas_vendidos():
 
 def get_presentaciones_mas_vendidas():
     """
-    Obtiene las presentaciones más vendidas.
-    Retorna una lista de nombres de presentaciones y una lista de cantidades vendidas.
+    Obtiene las presentaciones más vendidas
+    lista de nombres de presentaciones y una lista de cantidades vendidas
     """
-    # Usar el ORM para obtener las presentaciones más vendidas
+    #presentaciones más vendidas
     result = (db.session.query(TipoGalleta.nombre, db.func.sum(DetalleVentaGalletas.cantidad).label('cantidad_vendida'))
               .join(Galleta, Galleta.tipo_galleta_id == TipoGalleta.id_tipo_galleta)
               .join(LoteGalletas, LoteGalletas.galleta_id == Galleta.id_galleta)
